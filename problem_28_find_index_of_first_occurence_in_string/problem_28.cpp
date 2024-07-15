1f4f1725843e9a573723d121e7ffb63cc5044f0a
Romain Groux <romaingroux@users.noreply.github.com>
2024-07-15 09:23:32 +0200

    added python solution

--- problem_28_find_index_of_first_occurence_in_string/problem_28.cpp
+++ problem_28_find_index_of_first_occurence_in_string/problem_28.cpp
@@ -25,6 +25,7 @@ haystack and needle consist of only lowercase English characters.
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 #include <cassert>
 
 
@@ -40,7 +41,7 @@ std::ostream& operator << (std::ostream& stream,
 class Solution
 {
     public:
-    int strStrOld(std::string word, std::string pattern)
+    int strStrBruteForce(std::string word, std::string pattern)
     {   
         if(word.size() == 0)
         {   return -1 ; }
@@ -53,11 +54,6 @@ class Solution
             bool found = true ;
             for(size_t j=0; j<pattern.size(); j++)
             {   
-                std::cerr << "i: " << i << " " << " j: " << j << std::endl ;
-                std::cerr << "word:    " << word[i+j] << std::endl ;
-                std::cerr << "pattern: " << pattern[j] << std::endl ;
-                std::cerr << std::endl ;
-
                 if(word[i+j] != pattern[j])
                 {   i++ ;
                     found = false ;
@@ -72,12 +68,82 @@ class Solution
         return -1 ;
     }
 
-    int strStr(std::string word, std::string pattern)
-    {   int i = word.find(pattern) ; 
+    int strStrSTL(std::string word, std::string pattern)
+    {   size_t i = word.find(pattern) ; 
         if(i == std::string::npos)
         {   return -1 ; }
         return i ;
-    
+    }
+
+
+    int hash(const std::string& s, int radix, int mod)
+    {
+        int h = 0 ;
+        int l = s.size() ;
+
+        for(int i=0; i<l; i++)
+        {   
+            int h_part = (((s[i] - 'a') * (static_cast<int>(pow(radix, l-i-1)))) % mod)  ;
+            std::cerr << "contribution s[" << i << "] " << s[i] << " = " << h_part << " : " << s[i] << "- 'a'* pow(" << radix << "," << l << "-" << i << "-1)" << std::endl ;
+            h += h_part;
+        }
+        std::cerr << std::endl ;
+        return h ;
+    }
+
+    int strStrHash(std::string word, std::string pattern)
+    {   
+        if(word.size() == 0)
+        {   return -1 ; }
+
+        if(word.size() < pattern.size())
+        {   return -1 ; }
+
+        // constants for hashing
+        int radix = 26;
+        int mod = 
+        ;
+        // long max_weight = 1;
+
+        std::string word_0 = word.substr(0, 4) ;
+
+        int h_word_0 = hash(word_0, radix, mod) ;
+
+        std::cerr << word_0 << " " << h_word_0 << std::endl ;
+
+
+
+
+
+        /*
+        size_t l_pattern = pattern.size() ;
+        size_t l_word    = word.size() ;
+        std::string subword = word.substr(0, l_pattern) ;
+
+        int h_pattern = hash(pattern, radix, mod) ;
+        int h_subword = hash(subword, radix, mod) ;
+        
+        
+        if(h_pattern == h_subword)
+        {   return 0 ; }
+
+
+        for(size_t i=1; i<l_word-l_pattern+1; i++)
+        {   
+            subword = word.substr(i, l_pattern) ;
+
+            // update hash subword
+            h_pattern -= (((word[i-1] - 'a') * 
+                            (static_cast<int>(pow(radix, l_pattern-0-1)))) % mod) ;
+            h_pattern += (((word[i+l_pattern] - 'a') * 
+                            (static_cast<int>(pow(radix, l_pattern-(l_pattern-1)-1))))             % mod) ;
+
+            std::cerr << "-" << word[i-1] << " " << subword << " +" << word[i+l_pattern] << " " << h_pattern << " " << hash(subword, radix, mod) << std::endl ;
+        }
+
+        int pattern_hash = hash(pattern, radix, mod) ;
+        */
+        return -1 ;
     }
 } ;
 
@@ -86,11 +152,13 @@ int main()
 {
     Solution s ;
     
-    // assert(s.strStr( "sadbutsad", "sad") == 0) ;
-    // assert(s.strStr( "leetcode", "leeto") == -1) ;
-    // assert(s.strStr( "a", "a") == 0) ;
-    // assert(s.strStr( "", "leeto") == -1) ;
-    assert(s.strStr( "mississippi", "issip") == 4) ;
+    // assert(s.strStrSTL( "sadbutsad", "sad") == 0) ;
+    // assert(s.strStrSTL( "leetcode", "leeto") == -1) ;
+    // assert(s.strStrSTL( "a", "a") == 0) ;
+    // assert(s.strStrSTL( "", "leeto") == -1) ;
+    // assert(s.strStrSTL( "mississippi", "issip") == 4) ;
+
+    s.strStrHash( "mississippi", "issip") ;
 
     return 0 ;
 } ;
