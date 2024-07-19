d887b33a7966acb7791316dd292c569da374c2bb
Romain Groux <romaingroux@users.noreply.github.com>
2024-07-19 11:56:44 +0200

    cleaned code

--- problem_55_jump_game/problem_55.cpp
+++ problem_55_jump_game/problem_55.cpp
@@ -1,5 +1,7 @@
 /*
-You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+You are given an integer array nums. You are initially positioned at the 
+array's first index, and each element in the array represents your maximum 
+jump length at that position.
 
 Return true if you can reach the last index, or false otherwise.
 
@@ -14,7 +16,8 @@ Example 2:
 
 Input: nums = [3,2,1,0,4]
 Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump 
+length is 0, which makes it impossible to reach the last index.
  
 
 Constraints:
