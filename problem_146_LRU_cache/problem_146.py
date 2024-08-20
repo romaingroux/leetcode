9c9a7d4cd0842389e691fa3cf371f70eb1748e56
Romain Groux <romaingroux@users.noreply.github.com>
2024-08-20 17:02:46 +0200

    ../.vscode/settings.json devcontainer.json

--- problem_146_LRU_cache/problem_146.py
+++ problem_146_LRU_cache/problem_146.py
@@ -50,7 +50,10 @@ import typing as tp
 
 
 class ListNode:
-    
+    """
+    Implements a double linked list node.
+    """
+
     def __init__(self,
                  key: int,
                  value: int,
@@ -93,49 +96,10 @@ class ListNode:
         """
         return hash(str(self))
 
-
-def make_list(values: tp.List[tp.Tuple[int,int]]) \
-    -> tp.Optional[ListNode]:
-    """
-    Create a linked list with the given values and return the 
-    head node.
-    Args:
-        values: the values of the nodes.
-    Returns:
-        The head of the list.
-    """
-    # prehead
-    prehead = ListNode(-999,-999)
-    cur = prehead
-
-    for i in range(0,len(values)):
-        cur.next = ListNode(values[i][0],
-                            values[i][1],
-                            cur)
-        cur = cur.next
-    
-    head = prehead.next
-    head.prev = None
-    return head
-
-
-def print_list(head: tp.Optional[ListNode]) \
-    -> None:
-    """
-    Prints a linked list on stdout.
-    Args:
-        head: the head node of the list.
-    Returns:
-        None
-    """
-    s = []
-    while head:
-        s.append(str(head))
-        head = head.next
-    print(",".join(s))
-
-
 class LRUCache:
+    """
+    Implements a LRU cache.
+    """
 
     def __init__(self, 
                  capacity: int):
@@ -145,15 +109,15 @@ class LRUCache:
             capacity: the LRUCache capacity.
         """
 
-        """Pointer to most ancient LRUCache item (list head)"""
-        self.head = None
-        """Pointer to most recent LRUCache item (list tail)"""
-        self.tail = None
+        """Prehead to LRUCache item list (next item is most ancient)"""
+        self.prehead = ListNode(-1,-1,None,None)
+        """Posttail to LRUCache item list (prev item is most recent)"""
+        self.posttail = self.prehead.next
         """LRUCache dictionary"""
         self.dict = dict()
         """Capacity"""
         self.capacity = capacity
-        """Current number of pairs"""
+        """Current number of items in LRUCache"""
         self.size = 0
     
     def __str__(self) \
@@ -164,25 +128,73 @@ class LRUCache:
             A string representation of the instance.
         """
         l = []
-        l.append(f"{self.size}/{self.capacity}")
-        node = self.head
-        while node:
-            l.append(str(node))
-            node = node.next
+        l.append(f"{self.size}/{self.capacity},PREHEAD")
+        if self.size > 0:
+            node = self.prehead.next
+            for i in range(0,self.size):
+                l.append(str(node))
+                node = node.next
+        l.append(f"POSTTAIL")
         return ",".join(l)
+
+    def _pop(self, 
+             key:int) \
+        -> tp.Optional[ListNode]:
+        """
+        Pops the pair with the given key from the list and dict and 
+        returns it. Does nothing and returns None if no such key is present.
+        Args:
+            key: the key of interest.
+        Returns:
+            The popped node, None if no pair with this key was found.
+        """
+        node = self.dict.get(key,None)
+        if node:
+            node_prev = node.prev
+            node_next = node.next
+            node_prev.next = node_next
+            node_next.prev = node_prev
+            del self.dict[key]
+            return node
+        else:
+            return None
+    
+    def _append(self, 
+                node: ListNode) \
+        -> None:
+        """
+        Appends the given node after the current tail updates the list and dict.
+        Args:
+            node: the node of interest.
+        Returns:
+            None.
+        """
+        node_prev = self.posttail.prev
+        node_prev.next = node
+        node.prev = node_prev
+        node.next = self.posttail
+        self.posttail.prev = node
+        self.dict[node.key] = node
 
     def get(self, 
             key: int) \
         -> int:
         """
-        Returns the item with the value corresponding to this key.
+        Returns the item with the value corresponding to this key and updates 
+        the LRUCache.
         Args:
             key: the key of interest.
         Returns:
             The corresponding value, -1 if the key could not be found.
         """
-        node = self.dict.get(key,None)
-        return node.value if node else -1
+        if self.dict.get(key,None):
+            # pop node
+            node = self._pop(key)
+            # append node as new tail
+            self._append(node)
+            return self.posttail.prev.value
+        else:
+            return -1
 
     def put(self, 
             key: int, 
@@ -203,31 +215,47 @@ class LRUCache:
             # this key is not present yet
             if self.dict.get(key,None) is None:
                 # add in list
-                node = ListNode(key,value,self.tail,None)
-                if self.head is None:
-                    self.head = node
-                    self.tail = node
+                node = ListNode(key,value,None,None)
+                # LRUCache is empty
+                if self.size == 0:
+                    # new posttail
+                    self.posttail = ListNode(-1,-1,None,None)
+                    # new head
+                    node.prev = self.prehead
+                    node.next = self.posttail
+                    self.prehead.next = node
+                    self.posttail.prev = node
+                    # add in dict
+                    self.dict[key] = node
                 else:
-                    self.tail.next = node
-                    self.tail = self.tail.next
-                # add in dict
-                self.dict[key] = self.tail
+                    # append node as new tail
+                    self._append(node)
                 # increase size
                 self.size += 1
             # update value
             else:
-                node = self.dict[key]
+                # pop node and update value
+                node = self._pop(key)
                 node.value = value
-        # delete most ancient item
+                # append node as new tail
+                self._append(node)
+        # full
         else:
-            node = ListNode(key,value,self.tail,None)
-            # remove from dir
-            del self.dict[self.head.key]
-            # new head in list
-            self.head = self.head.next
-            # new tail
-            self.tail.next = node
-            self.tail = self.tail.next
+            # this key is not present yet, replace most ancient item
+            if self.dict.get(key,None) is None:
+                # pop head
+                self._pop(self.prehead.next.key)
+                node = ListNode(key,value,None,None)
+                # append node as new tail
+                self._append(node)
+            # update value
+            else:
+                # pop node and update value
+                node = self._pop(key)
+                node.value = value
+                # append node as new tail
+                self._append(node)
+
     
     def to_array(self) \
         -> tp.List[tp.Tuple[int,int]]:
@@ -238,44 +266,29 @@ class LRUCache:
             An array containing the key-value pairs of the instance.
         """
         l = []
-        node = self.head
-        while node:
-            l.append((node.key,node.value))
-            node = node.next
+        if self.size > 0:
+            node = self.prehead.next
+            for i in range(0,self.size):
+                l.append((node.key,node.value))
+                node = node.next
         return l
 
 if __name__ == "__main__":
-
-    # Your LRUCache object will be instantiated and called as such:
-    # obj = LRUCache(capacity)
-    # param_1 = obj.get(key)
-    # obj.put(key,value)
-
-    # cache = LRUCache(4)
-    # cache.put(1,2)
-    # print(cache)
-    # cache.put(3,4)
-    # print(cache)
-    # cache.put(5,6)
-    # print(cache)
-    # cache.put(7,8)
-    # print(cache)
-    # cache.put(9,10)
-    # print(cache)
-    # cache.put(11,12)
-    # print(cache)
     
-    lRUCache = LRUCache(2);
-    lRUCache.put(1, 1)          #  cache is {1=1}
-    assert(lRUCache.to_array() == [(1,1)])
-    lRUCache.put(2, 2)          # cache is {1=1, 2=2}
-    assert(lRUCache.to_array() == [(1,1),(2,2)])
-    assert(lRUCache.get(1)== 1) #  return 1
-    lRUCache.put(3, 3)          #  LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-    assert(lRUCache.to_array() == [(1,1),(3,3)])
-    # assert(lRUCache.get(2) == -1)        #  returns -1 (not found)
-    # lRUCache.put(4, 4) ; print(lRUCache) #  LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-    # lRUCache.get(1)                      #  return -1 (not found)
-    # lRUCache.get(3)                      #  return 3
-    # lRUCache.get(4)                      #  return 4
 
+    #           ["LRUCache","get","put","get","put","put","get","get"]
+    # parameter [[2],        [2], [2,6], [1],  [1,5],[1,2],[1],  [2]]
+    # expected  [null,       -1,   null, -1,   null,  null, 2,    6]
+    lRUCache = LRUCache(2)
+    assert(lRUCache.get(2) == -1)
+    lRUCache.put(2,6)
+    assert(lRUCache.to_array() == [(2,6)])
+    assert(lRUCache.get(1) == -1)
+    lRUCache.put(1,5)
+    assert(lRUCache.to_array() == [(2,6),(1,5)])
+    lRUCache.put(1,2)
+    assert(lRUCache.to_array() == [(2,6),(1,2)])
+    assert(lRUCache.get(1) == 2)
+    assert(lRUCache.to_array() == [(2,6),(1,2)])
+    assert(lRUCache.get(2) == 6)
+    assert(lRUCache.to_array() == [(1,2),(2,6)])
